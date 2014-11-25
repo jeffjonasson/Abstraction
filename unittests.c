e249f472f8319e44bf234f17fadc2b9e6e6e7c3d
jeffjonasson <jeff.jonasson@gmail.com>
2014-11-25 16:36:54 +0100

    added testfile

---
+++ unittests.c
@@ -0,0 +1,61 @@
+#include "CUnit/Basic.h"
+#include <stdlib.h>
+
+int init_suite_1(void){
+  return 0;
+}
+
+int clean_suite_1(void){
+  return 0;
+}
+
+int init_suite_2(void){
+  return 0;
+}
+
+int clean_suite_2(void){
+  return 0;
+}
+
+void testFive(){
+  int temp = 5;
+  CU_ASSERT(temp == 5);
+}
+
+int main(){
+  CU_pSuite pSuite1 = NULL;
+  CU_pSuite pSuite2 = NULL;
+
+  /* initialize the CUnit test registry */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+
+  /* add a suites to the registry */
+  pSuite1 = CU_add_suite("Basic Functions Suite", init_suite_1, clean_suite_1);
+  if (NULL == pSuite1)
+    {
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+  pSuite2 = CU_add_suite("Advanced Functions Suite", init_suite_2, clean_suite_2);
+  if (NULL == pSuite2)
+    {
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+
+   if (
+    (NULL == CU_add_test(pSuite1, "test if test works", returnFive))
+  )
+    {
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+
+  /* Run all tests using the CUnit Basic interface */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  CU_basic_run_tests();
+  CU_cleanup_registry();
+  return CU_get_error();
+
+}
